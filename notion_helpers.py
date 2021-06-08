f74d5c743d1518a54f200482b237032a18b4fdfe
Avi Udash <udashavi@gmail.com>
2021-06-08 00:29:37 -0500

    notion create_page with just album name works now

--- notion_helpers.py
+++ notion_helpers.py
@@ -1,1 +1,36 @@
-import requests, json
+import requests
+import json
+import os
+
+# get the notion env variables defined in a .env file
+from dotenv import load_dotenv
+load_dotenv()
+NOTION_TOKEN = os.getenv('NOTION_TOKEN')
+NOTION_DATABASE = os.getenv('NOTION_DATABASE')
+
+# create requests important info
+url = 'https://api.notion.com/v1/pages'
+req_headers = {
+    'Authorization': 'Bearer {}'.format(NOTION_TOKEN),
+    'Content-Type': 'application/json',
+    'Notion-Version': '2021-05-13'
+}
+
+# creates a page in the database with the given arguments
+
+
+def create_page(album_name, artists, released):
+    body = {
+        "parent": {
+            "type": "database_id",
+            "database_id": NOTION_DATABASE
+        },
+        "properties": {
+            "Album Title": {
+                "type": "title",
+                "title": [{"type": "text", "text": {"content": album_name}}]
+            }
+        }
+    }
+
+    r = requests.post(url, headers=req_headers, data=json.dumps(body))
